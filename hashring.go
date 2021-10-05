eabad103d342e706ba7d9e244acf97e518cf76ba
Xue Jiao <jiao.xue@seamoney.com>
2021-10-05 16:24:13 +0800

    refactor hashVal

--- hashring.go
+++ hashring.go
@@ -8,7 +8,6 @@ import (
 )
 
 type HashKey uint32
-
 type HashKeyOrder []HashKey
 
 func (h HashKeyOrder) Len() int           { return len(h) }
@@ -48,19 +47,6 @@ func NewWithWeights(weights map[string]int) *HashRing {
 	return hashRing
 }
 
-func hashVal(bKey []byte, entryFn func(int) int) HashKey {
-	return ((HashKey(bKey[entryFn(3)]) << 24) |
-		(HashKey(bKey[entryFn(2)]) << 16) |
-		(HashKey(bKey[entryFn(1)]) << 8) |
-		(HashKey(bKey[entryFn(0)])))
-}
-
-func hashDigest(key string) []byte {
-	m := md5.New()
-	m.Write([]byte(key))
-	return m.Sum(nil)
-}
-
 func (h *HashRing) generateCircle() {
 	totalWeight := 0
 	for _, node := range h.nodes {
@@ -85,7 +71,7 @@ func (h *HashRing) generateCircle() {
 			bKey := hashDigest(nodeKey)
 
 			for i := 0; i < 3; i++ {
-				key := hashVal(bKey, func(x int) int { return x + i*4 })
+				key := hashVal(bKey[i*4 : i*4+4])
 				h.ring[key] = node
 				h.sortedKeys = append(h.sortedKeys, key)
 			}
@@ -123,7 +109,7 @@ func (h *HashRing) GetNodePos(stringKey string) (pos int, ok bool) {
 
 func (h *HashRing) GenKey(key string) HashKey {
 	bKey := hashDigest(key)
-	return hashVal(bKey, func(x int) int { return x })
+	return hashVal(bKey[0:4])
 }
 
 func (h *HashRing) AddNode(node string) *HashRing {
@@ -184,4 +170,17 @@ func (h *HashRing) RemoveNode(node string) *HashRing {
 	}
 	hashRing.generateCircle()
 	return hashRing
+}
+
+func hashVal(bKey []byte) HashKey {
+	return ((HashKey(bKey[3]) << 24) |
+		(HashKey(bKey[2]) << 16) |
+		(HashKey(bKey[1]) << 8) |
+		(HashKey(bKey[0])))
+}
+
+func hashDigest(key string) []byte {
+	m := md5.New()
+	m.Write([]byte(key))
+	return m.Sum(nil)
 }
